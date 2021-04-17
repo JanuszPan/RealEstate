c65bed7d325cf4c58adad2202ed571bf42950015
JanuszPan <janusz.panczyk@gmail.com>
2021-04-17 11:29:29 +0200

    index_path

--- src/main/java/controller/IndexController.java
+++ src/main/java/controller/IndexController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
-    @GetMapping(path = "/hello", produces = "text/html; charset=UTF-8")
+    @GetMapping(path = "/", produces = "text/html; charset=UTF-8")
     public String hello() {
         return "index";
     }
